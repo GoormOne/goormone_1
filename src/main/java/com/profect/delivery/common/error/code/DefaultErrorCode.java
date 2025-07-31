01ccda7ae5ef01fe914a8ab8e980210b680c30ee
kimsumin <96856816+bradleysoo@users.noreply.github.com>
2025-07-31 19:04:29 +0900

    Revert "[FEAT] #6: 리뷰 조회, 삭제, 수정 기능 구현"

--- src/main/java/com/profect/delivery/common/error/code/DefaultErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.profect.delivery.common.error.code;
-
-import org.springframework.http.HttpStatus;
-
-public interface DefaultErrorCode {
-    HttpStatus getHttpStatus();
-    String getMessage();
-
-}
