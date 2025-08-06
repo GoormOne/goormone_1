913be5f9902bd9180a50a372395c13cb6e31525e
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2025-08-06 09:04:08 +0900

    fix: Change the application.yml

--- src/main/resources/static/init.sql
+++ src/main/resources/static/init.sql
@@ -23,82 +23,82 @@ CREATE TYPE "payment_result" AS ENUM (
 );
 
 CREATE TABLE "p_users" (
-  "user_id" varchar(10) NOT NULL PRIMARY KEY,
-  "username" varchar(10) UNIQUE NOT NULL,
-  "password" varchar(15) NOT NULL,
-  "name" varchar(10) NOT NULL,
-  "birth" date NOT NULL,
-  "email" varchar(30) UNIQUE NOT NULL,
-  "role" role_type NOT NULL,
-  "is_public" boolean NOT NULL,
-  "is_banned" boolean NOT NULL,
-  "created_at" timestamp(0) NOT NULL,
-  "created_by" varchar(10) NOT NULL,
-  "updated_at" timestamp(0),
-  "updated_by" varchar(10),
-  "deleted_at" timestamp(0),
-  "deleted_by" varchar(10),
-  "deleted_rs" varchar(100)
+                           "user_id" varchar(10) NOT NULL PRIMARY KEY,
+                           "username" varchar(10) UNIQUE NOT NULL,
+                           "password" varchar(60) NOT NULL,
+                           "name" varchar(10) NOT NULL,
+                           "birth" date NOT NULL,
+                           "email" varchar(30) UNIQUE NOT NULL,
+                           "role" role_type NOT NULL,
+                           "is_public" boolean NOT NULL,
+                           "is_banned" boolean NOT NULL,
+                           "created_at" timestamp(0) NOT NULL,
+                           "created_by" varchar(10) NOT NULL,
+                           "updated_at" timestamp(0),
+                           "updated_by" varchar(10),
+                           "deleted_at" timestamp(0),
+                           "deleted_by" varchar(10),
+                           "deleted_rs" varchar(100)
 );
 
 CREATE TABLE "p_customer_address" (
-  "address_cd" uuid NOT NULL PRIMARY KEY,
-  "user_id" varchar(10) NOT NULL,
-  "address_name" varchar(20) NOT NULL,
-  "address1" varchar(50) NOT NULL,
-  "address2" varchar(50) NOT NULL,
-  "zip_cd" varchar(6) NOT NULL,
-  "user_latitude" decimal(10,6) NOT NULL,
-  "user_longitude" decimal(10,6) NOT NULL,
-  "is_default" boolean NOT NULL,
-  "created_at" timestamp(0) NOT NULL,
-  "updated_at" timestamp(0),
-  "deleted_at" timestamp(0)
+                                      "address_cd" uuid NOT NULL PRIMARY KEY,
+                                      "user_id" varchar(10) NOT NULL,
+                                      "address_name" varchar(20) NOT NULL,
+                                      "address1" varchar(50) NOT NULL,
+                                      "address2" varchar(50) NOT NULL,
+                                      "zip_cd" varchar(6) NOT NULL,
+                                      "user_latitude" decimal(10,6) NOT NULL,
+                                      "user_longitude" decimal(10,6) NOT NULL,
+                                      "is_default" boolean NOT NULL,
+                                      "created_at" timestamp(0) NOT NULL,
+                                      "updated_at" timestamp(0),
+                                      "deleted_at" timestamp(0)
 );
 
 CREATE TABLE "p_regions" (
-  "region_id" uuid NOT NULL PRIMARY KEY,
-  "region_1depth_name" varchar(50) NOT NULL,
-  "region_2depth_name" varchar(50) NOT NULL,
-  "region_3depth_name" varchar(50) NOT NULL,
-  "created_at" timestamp(0) NOT NULL,
-  "updated_at" timestamp(0),
-  "deleted_at" timestamp(0)
+                             "region_id" uuid NOT NULL PRIMARY KEY,
+                             "region_1depth_name" varchar(50) NOT NULL,
+                             "region_2depth_name" varchar(50) NOT NULL,
+                             "region_3depth_name" varchar(50) NOT NULL,
+                             "created_at" timestamp(0) NOT NULL,
+                             "updated_at" timestamp(0),
+                             "deleted_at" timestamp(0)
 );
 
 CREATE TABLE "p_stores_category" (
-  "stores_category_id" uuid NOT NULL PRIMARY KEY,
-  "stores_category" varchar(30) NOT NULL
+                                     "stores_category_id" uuid NOT NULL PRIMARY KEY,
+                                     "stores_category" varchar(30) NOT NULL
 );
 
 CREATE TABLE "p_stores" (
-    store_id            UUID PRIMARY KEY,
-    user_id             VARCHAR(10) NOT NULL,
-    stores_category_id  UUID NOT NULL,
-    store_name          VARCHAR(30) NOT NULL,
-    store_description   TEXT NOT NULL,
-    address1            VARCHAR(50) NOT NULL,
-    address2            VARCHAR(50) NOT NULL,
-    zip_cd              VARCHAR(6) NOT NULL,
-    store_phone         VARCHAR(15) NOT NULL,
-    store_latitude      DECIMAL(10,6) NOT NULL,
-    store_longitude     DECIMAL(10,6) NOT NULL,
-    open_time           TIME NOT NULL,
-    close_time          TIME NOT NULL,
-    is_banned           BOOLEAN NOT NULL,
-    created_at          TIMESTAMP(0) NOT NULL,
-    created_by          VARCHAR(10) NOT NULL,
-    updated_at          TIMESTAMP(0),
-    updated_by          VARCHAR(10),
-    deleted_at          TIMESTAMP(0),
-    deleted_by          VARCHAR(10),
-    deleted_rs          VARCHAR(100)
+                            store_id            UUID PRIMARY KEY,
+                            user_id             VARCHAR(10) NOT NULL,
+                            stores_category_id  UUID NOT NULL,
+                            store_name          VARCHAR(30) NOT NULL,
+                            store_description   TEXT NOT NULL,
+                            address1            VARCHAR(50) NOT NULL,
+                            address2            VARCHAR(50) NOT NULL,
+                            zip_cd              VARCHAR(6) NOT NULL,
+                            store_phone         VARCHAR(15) NOT NULL,
+                            store_latitude      DECIMAL(10,6) NOT NULL,
+                            store_longitude     DECIMAL(10,6) NOT NULL,
+                            open_time           TIME NOT NULL,
+                            close_time          TIME NOT NULL,
+                            is_banned           BOOLEAN NOT NULL,
+                            created_at          TIMESTAMP(0) NOT NULL,
+                            created_by          VARCHAR(10) NOT NULL,
+                            updated_at          TIMESTAMP(0),
+                            updated_by          VARCHAR(10),
+                            deleted_at          TIMESTAMP(0),
+                            deleted_by          VARCHAR(10),
+                            deleted_rs          VARCHAR(100)
 );
 
 CREATE TABLE "p_stores_regions" (
-  "store_id" uuid NOT NULL,
-  "region_id" uuid NOT NULL,
-  "created_at" timestamp(0) NOT NULL
+                                    "store_id" uuid NOT NULL,
+                                    "region_id" uuid NOT NULL,
+                                    "created_at" timestamp(0) NOT NULL
 );
 
 -- CREATE TABLE "p_stores_business_hr" (
@@ -110,31 +110,31 @@ CREATE TABLE "p_stores_regions" (
 -- );
 
 CREATE TABLE "p_menu_category" (
-  "menu_category_id" uuid NOT NULL PRIMARY KEY,
-  "store_id" uuid NOT NULL,
-  "menu_category_name" varchar(20) NOT NULL,
-  "created_at" timestamp(0) NOT NULL,
-  "updated_at" timestamp(0),
-  "deleted_at" timestamp(0)
+                                   "menu_category_id" uuid NOT NULL PRIMARY KEY,
+                                   "store_id" uuid NOT NULL,
+                                   "menu_category_name" varchar(20) NOT NULL,
+                                   "created_at" timestamp(0) NOT NULL,
+                                   "updated_at" timestamp(0),
+                                   "deleted_at" timestamp(0)
 );
 
 CREATE TABLE "p_menus" (
-  "menu_id" uuid NOT NULL PRIMARY KEY,
-  "store_id" uuid NOT NULL,
-  "menu_category_id" uuid NOT NULL,
-  "menu_name" varchar(20) NOT NULL,
-  "menu_price" int NOT NULL,
-  "menu_description" text NOT NULL,
-  "is_public" boolean NOT NULL,
-  "menu_photo_url" varchar(100),
-  "is_public_photo" boolean,
-  "created_at" timestamp(0) NOT NULL,
-  "created_by" varchar(10) NOT NULL,
-  "updated_at" timestamp(0),
-  "updated_by" varchar(10),
-  "deleted_at" timestamp(0),
-  "deleted_by" varchar(10),
-  "deleted_rs" varchar(100)
+                           "menu_id" uuid NOT NULL PRIMARY KEY,
+                           "store_id" uuid NOT NULL,
+                           "menu_category_id" uuid NOT NULL,
+                           "menu_name" varchar(20) NOT NULL,
+                           "menu_price" int NOT NULL,
+                           "menu_description" text NOT NULL,
+                           "is_public" boolean NOT NULL,
+                           "menu_photo_url" varchar(100),
+                           "is_public_photo" boolean,
+                           "created_at" timestamp(0) NOT NULL,
+                           "created_by" varchar(10) NOT NULL,
+                           "updated_at" timestamp(0),
+                           "updated_by" varchar(10),
+                           "deleted_at" timestamp(0),
+                           "deleted_by" varchar(10),
+                           "deleted_rs" varchar(100)
 );
 
 -- CREATE TABLE "p_menu_photos" (
@@ -152,99 +152,101 @@ CREATE TABLE "p_menus" (
 -- );
 
 CREATE TABLE "p_carts" (
-  "cart_id" uuid NOT NULL PRIMARY KEY,
-  "store_id" uuid NOT NULL,
-  "user_id" varchar(10) NOT NULL,
-  "cart_status" cart_status NOT NULL,
-  "created_at" timestamp(0) NOT NULL,
-  "updated_at" timestamp(0),
-  "updated_by" varchar(10)
+                           "cart_id" uuid NOT NULL PRIMARY KEY,
+                           "store_id" uuid NOT NULL,
+                           "user_id" varchar(10) NOT NULL,
+                           "cart_status" cart_status NOT NULL,
+                           "created_at" timestamp(0) NOT NULL,
+                           "updated_at" timestamp(0),
+                           "updated_by" varchar(10)
 );
 
 CREATE TABLE "p_cart_items" (
-  "cart_item_id" uuid NOT NULL PRIMARY KEY,
-  "cart_id" uuid NOT NULL,
-  "menu_id" uuid NOT NULL,
-  "quantity" int NOT NULL,
-  "created_at" timestamp(0) NOT NULL,
-  "updated_at" timestamp(0),
-  "updated_by" varchar(10)
+                                "cart_item_id" uuid NOT NULL PRIMARY KEY,
+                                "cart_id" uuid NOT NULL,
+                                "menu_id" uuid NOT NULL,
+                                "quantity" int NOT NULL,
+                                "created_at" timestamp(0) NOT NULL,
+                                "updated_at" timestamp(0),
+                                "updated_by" varchar(10)
 );
 
 CREATE TABLE "p_orders" (
-  "order_id" uuid NOT NULL PRIMARY KEY,
-  "user_id" varchar(10) NOT NULL,
-  "store_id" uuid NOT NULL,
-  "address_cd" uuid NOT NULL,
-  "cart_id" uuid NOT NULL,
-  "total_price" int NOT NULL,
-  "request_message" text,
-  "created_at" timestamp(0) NOT NULL,
-  "created_by" varchar(10) NOT NULL,
-  "deleted_at" timestamp(0),
-  "deleted_by" varchar(10),
-  "deleted_rs" varchar(100)
+                            "order_id" uuid NOT NULL PRIMARY KEY,
+                            "user_id" varchar(10) NOT NULL,
+                            "store_id" uuid NOT NULL,
+                            "address_cd" uuid NOT NULL,
+                            "cart_id" uuid NOT NULL,
+                            "total_price" int NOT NULL,
+                            "request_message" text,
+                            "created_at" timestamp(0) NOT NULL,
+                            "created_by" varchar(10) NOT NULL,
+                            "deleted_at" timestamp(0),
+                            "deleted_by" varchar(10),
+                            "deleted_rs" varchar(100)
 );
 
 CREATE TABLE "p_order_items" (
-  "order_item_id" uuid NOT NULL PRIMARY KEY,
-  "order_id" uuid NOT NULL,
-  "menu_id" uuid NOT NULL,
-  "quantity" int NOT NULL,
-  "created_at" timestamp(0) NOT NULL,
-  "updated_at" timestamp(0),
-  "updated_by" varchar(10)
+                                 "order_item_id" uuid NOT NULL PRIMARY KEY,
+                                 "order_id" uuid NOT NULL,
+                                 "menu_id" uuid NOT NULL,
+                                 "quantity" int NOT NULL,
+                                 "created_at" timestamp(0) NOT NULL,
+                                 "updated_at" timestamp(0),
+                                 "updated_by" varchar(10)
 );
 
 CREATE TABLE "p_reviews" (
-  "review_id" uuid NOT NULL PRIMARY KEY,
-  "store_id" uuid NOT NULL,
-  "user_id" varchar(10) NOT NULL,
-  "rating" smallint NOT NULL,
-  "comment" text,
-  "is_public" boolean NOT NULL,
-  "created_at" timestamp(0) NOT NULL,
-  "created_by" varchar(10) NOT NULL
+                             "review_id" uuid NOT NULL PRIMARY KEY,
+                             "store_id" uuid NOT NULL,
+                             "user_id" varchar(10) NOT NULL,
+                             "rating" smallint NOT NULL,
+                             "comment" text,
+                             "is_public" boolean NOT NULL,
+                             "created_at" timestamp(0) NOT NULL,
+                             "created_by" varchar(10) NOT NULL,
+                             "updated_at" timestamp(0),
+                             "updated_by" varchar(10)
 );
 
 CREATE TABLE "p_review_summary" (
-  "batch_id" uuid NOT NULL PRIMARY KEY,
-  "store_id" uuid NOT NULL,
-  "summary_refresh_date" date NOT NULL,
-  "summary_text" text NOT NULL,
-  "created_at" timestamp(0) NOT NULL,
-  "created_by" varchar(10) NOT NULL
+                                    "batch_id" uuid NOT NULL PRIMARY KEY,
+                                    "store_id" uuid NOT NULL,
+                                    "summary_refresh_date" date NOT NULL,
+                                    "summary_text" text NOT NULL,
+                                    "created_at" timestamp(0) NOT NULL,
+                                    "created_by" varchar(10) NOT NULL
 );
 
 CREATE TABLE "p_review_average" (
-    "store_id" uuid NOT NULL PRIMARY KEY,
-    "count" int,
-    "total" int
+                                    "store_id" uuid NOT NULL PRIMARY KEY,
+                                    "count" int,
+                                    "total" int
 );
 
 CREATE TABLE "p_payments" (
-  "payment_id" uuid NOT NULL PRIMARY KEY,
-  "user_id" varchar(10) NOT NULL,
-  "store_id" uuid NOT NULL,
-  "order_id" uuid NOT NULL,
-  "payment_method" payment_method NOT NULL,
-  "card_number" varchar(16) NOT NULL,
-  "payment_amount" int NOT NULL,
-  "payment_time" timestamp(0) NOT NULL,
-  "payment_result" payment_result NOT NULL,
-  "failure_reason" text
+                              "payment_id" uuid NOT NULL PRIMARY KEY,
+                              "user_id" varchar(10) NOT NULL,
+                              "store_id" uuid NOT NULL,
+                              "order_id" uuid NOT NULL,
+                              "payment_method" payment_method NOT NULL,
+                              "card_number" varchar(16) NOT NULL,
+                              "payment_amount" int NOT NULL,
+                              "payment_time" timestamp(0) NOT NULL,
+                              "payment_result" payment_result NOT NULL,
+                              "failure_reason" text
 );
 
 CREATE TABLE "p_errors" (
-  "error_id" uuid NOT NULL PRIMARY KEY,
-  "user_id" varchar(10) NOT NULL,
-  "request_url" varchar(255) NOT NULL,
-  "http_method" varchar(10) NOT NULL,
-  "error_code" varchar(20) NOT NULL,
-  "error_message" text NOT NULL,
-  "client_ip" varchar(45) NOT NULL,
-  "user_agent" text NOT NULL,
-  "created_at" timestamp(0) NOT NULL
+                            "error_id" uuid NOT NULL PRIMARY KEY,
+                            "user_id" varchar(10) NOT NULL,
+                            "request_url" varchar(255) NOT NULL,
+                            "http_method" varchar(10) NOT NULL,
+                            "error_code" varchar(20) NOT NULL,
+                            "error_message" text NOT NULL,
+                            "client_ip" varchar(45) NOT NULL,
+                            "user_agent" text NOT NULL,
+                            "created_at" timestamp(0) NOT NULL
 );
 
 COMMENT ON TABLE "p_stores_regions" IS '교차 테이블';
