9d0437d4ffe67406fbd0b0dd0eea10b5681a517c
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2025-08-06 12:31:06 +0900

    fix: Modify the application.yaml #40

--- src/main/java/com/profect/delivery/domain/store/repository/custom/MenuRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.profect.delivery.domain.store.repository.custom;
-
-import com.profect.delivery.domain.store.dto.response.StoreSearchDto;
-
-import java.util.List;
-
-public interface MenuRepositoryCustom {
-    List<StoreSearchDto> searchByKeyword(String keyword);
-}
